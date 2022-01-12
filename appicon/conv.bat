46f4244fe65cfcc7a0c21ef5ceba73d611534178
uhwang <43251090+uhwang@users.noreply.github.com>
2022-01-12 13:04:04 -0600

    Application icon

---
+++ appicon/conv.bat
@@ -0,0 +1,5 @@
+png2ico.exe readbib.ico readbib-icon-48x48.png readbib-icon-32x32.png readbib-icon-24x24.png readbib-icon-16x16.png
+magick readbib-icon-48x48.png icon_readbib.xpm
+copy /y icon_readbib.xpm icon_readbib.py
+copy /y readbib.ico ..
+copy /y icon_readbib.py ..
